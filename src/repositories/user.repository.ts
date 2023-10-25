432a966883cc351c445069b8e2837030e70ad807
chaerla <chelgchen@gmail.com>
2023-10-25 09:25:08 +0700

    feat: add user repository and service

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,41 @@
+import prisma from '@/database/prisma';
+
+export interface IInsertUser {
+  username: string;
+  password: string;
+  email: string;
+  name: string;
+}
+class UserRepository {
+  getUserById = async (id: number) => {
+    return await prisma.user.findFirst({
+      where: {
+        id,
+      },
+    });
+  };
+
+  getUserByUsername = async (username: string) => {
+    return await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+  };
+
+  getUserByEmail = async (email: string) => {
+    return await prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+  };
+
+  addUser = async (data: IInsertUser) => {
+    return await prisma.user.create({
+      data,
+    });
+  };
+}
+
+export default UserRepository;
---
+++ src/services/user.service.ts
@@ -0,0 +1,53 @@
+import UserRepository from '@/repositories/user.repository';
+import { RegisterDto } from '@/domain/dtos/register.dto';
+import bcrypt from 'bcrypt';
+import BadRequest from '@errors/bad-request.error';
+import Unauthorized from '@errors/unauthorized.error';
+import { LoginDto } from '@/domain/dtos/login.dto';
+import NotFound from '@errors/not-found.error';
+import { type User } from '@prisma/client';
+import { SECRET_KEY } from '@config';
+import jwt from 'jsonwebtoken';
+
+class UserService {
+  constructor(private userRepository: UserRepository) {}
+
+  findUserByUsername = async (username: string) => {
+    const user = await this.userRepository.getUserByUsername(username);
+    if (!user) {
+      throw new NotFound(`User with username ${username} not found`);
+    }
+    return user;
+  };
+  login = async ({ username, password }: LoginDto) => {
+    const user = await this.findUserByUsername(username);
+    if (!(await bcrypt.compare(password, user.password))) {
+      throw new Unauthorized('Invalid Credentials');
+    }
+
+    delete user.password;
+
+    return { auth: { user, access_token: await this.generateToken(user) } };
+  };
+
+  register = async (registerDto: RegisterDto) => {
+    if (await this.userRepository.getUserByUsername(registerDto.username)) {
+      throw new BadRequest('Validation Error', { username: 'Username is already taken' });
+    }
+    if (await this.userRepository.getUserByEmail(registerDto.email)) {
+      throw new BadRequest('Validation Error', { email: 'Email is already taken' });
+    }
+
+    registerDto.password = await bcrypt.hash(registerDto.password, 10);
+    const newUser = await this.userRepository.addUser(registerDto);
+    delete newUser.password;
+
+    return { auth: { user: newUser, access_token: await this.generateToken(newUser) } };
+  };
+
+  generateToken = async (user: User) => {
+    return await jwt.sign({ user }, SECRET_KEY, { expiresIn: '8h' });
+  };
+}
+
+export default UserService;
