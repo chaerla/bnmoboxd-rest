387d595cc0b3ae522f433a5a25fecc4a7421337c
chaerla <chelgchen@gmail.com>
2023-11-01 23:02:41 +0700

    feat: add axios

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import Application from '@/application';
 import AuthController from '@controllers/auth.controller';
 import IndexController from '@controllers/index.controller';
 import CuratorReviewController from '@controllers/curator-review.controller';
+import FilmController from '@controllers/film.controller';
 
-const app = new Application([new AuthController(), new IndexController(), new CuratorReviewController()]);
+const app = new Application([new AuthController(), new IndexController(), new CuratorReviewController(), new FilmController()]);
 app.listen();
