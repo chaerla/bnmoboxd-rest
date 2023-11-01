46277085c617b074c059c348936c25012b1f6623
chaerla <chelgchen@gmail.com>
2023-11-01 23:03:43 +0700

    feat: add register schema

--- src/domain/schemas/register.schema.ts
+++ src/domain/schemas/register.schema.ts
@@ -5,10 +5,14 @@ export const registerSchema: RequestSchema = {
   body: z.object({
     username: z
       .string({ required_error: 'Username cannot be empty' })
-      .min(5, { message: 'Username is too short' })
-      .max(20, { message: 'Username is too long' }),
+      .min(5, { message: 'Username should not be shorter than 5 characters' })
+      .max(16, { message: 'Username should not be longer than 16 characters' }),
     password: z.string({ required_error: 'Password cannot be empty' }).min(6, { message: 'Password must be at least 6 characters' }),
     email: z.string({ required_error: 'Email cannot be empty' }).email({ message: 'Invalid email format' }),
-    name: z.string({ required_error: 'Name cannot be empty' }).min(2, { message: 'Name is too short' }).max(255, { message: 'Name is too long' }),
+    firstName: z
+      .string({ required_error: 'First name cannot be empty' })
+      .min(2, { message: 'First name should be longer than 2 characters' })
+      .max(255, { message: 'Name is too long' }),
+    lastName: z.string().optional(),
   }),
 };
