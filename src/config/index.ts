077f2ac483875ec5142e2203bfe56b27ba81b037
chaerla <chelgchen@gmail.com>
2023-11-10 23:35:53 +0700

    feat: add configs

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv';
 config({ path: `.env` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { ENV, PORT, SECRET_KEY, DATABASE_URL, ORIGIN, PHP_BASE_URL } = process.env;
+export const { ENV, PORT, SECRET_KEY, DATABASE_URL, ORIGIN, PHP_BASE_URL, SOAP_BASE_URL, PHP_API_KEY, SOAP_API_KEY } = process.env;
