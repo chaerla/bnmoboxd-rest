6d72bf6fad3ae2be87a4f6c3ba18752fe97b0ec6
chaerla <chelgchen@gmail.com>
2023-11-10 23:36:26 +0700

    feat: registered new controllers

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import AuthController from '@controllers/auth.controller';
 import IndexController from '@controllers/index.controller';
 import CuratorReviewController from '@controllers/curator-review.controller';
 import FilmController from '@controllers/film.controller';
+import SubscriptionController from '@controllers/subscription.controller';
+import CuratorController from '@controllers/curator.controller';
 
-const app = new Application([new AuthController(), new IndexController(), new CuratorReviewController(), new FilmController()]);
+const app = new Application([
+  new AuthController(),
+  new IndexController(),
+  new CuratorReviewController(),
+  new FilmController(),
+  new SubscriptionController(),
+  new CuratorController(),
+]);
 app.listen();
