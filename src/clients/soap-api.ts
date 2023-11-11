83fd180c19b6732ab412b9237ba53e5fb55d953a
chaerla <chelgchen@gmail.com>
2023-11-11 19:05:58 +0700

    feat: add soap api

--- src/clients/soap-api.ts
+++ src/clients/soap-api.ts
@@ -1,9 +1,66 @@
-import { SOAP_BASE_URL } from '@config';
+import { REST_API_KEY, SOAP_BASE_URL } from '@config';
+import { createClient } from 'soap';
+import ApplicationError from '@errors/application.error';
 
 class SoapApi {
   private BASE_URL = SOAP_BASE_URL;
-
+  private SUBSCRIPTION_ENDPOINT = '/subscription';
   constructor() {}
+  private postSubscriptionReq = async (funcName: string, payload: any) => {
+    try {
+      const url = `${this.BASE_URL}${this.SUBSCRIPTION_ENDPOINT}?wsdl`;
+
+      const client = await this.createSoapClient(url);
+      const result = await this.invokeSoapFunction(client, funcName, payload);
+      console.log(result);
+      return result?.return || null;
+    } catch (error) {
+      console.log(error);
+      throw new ApplicationError();
+    }
+  };
+
+  private createSoapClient = (url: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      createClient(url, (err: any, client: any) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(client);
+        }
+      });
+    });
+  };
+
+  private invokeSoapFunction = (client: any, funcName: string, payload: any): Promise<any> => {
+    return new Promise((resolve, reject) => {
+      const header = {
+        'x-api-key': REST_API_KEY,
+      };
+
+      client[funcName](
+        payload,
+        (err: any, result: any) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        },
+        null,
+        header,
+      );
+    });
+  };
+
+  getAllSubscriptions = async () => {
+    const funcName = 'getAll';
+    const payload = {};
+
+    const response = await this.postSubscriptionReq(funcName, payload);
+    console.log(response);
+    return response;
+  };
 }
 
 export default SoapApi;
