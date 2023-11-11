08cb4cf5d35f136b1100778814b97a98731f2d27
chaerla <chelgchen@gmail.com>
2023-11-11 19:07:19 +0700

    feat: add curator details endpoint

--- src/controllers/curator.controller.ts
+++ src/controllers/curator.controller.ts
@@ -1,11 +1,11 @@
 import Controller from '@interfaces/controller';
-import { Router } from 'express';
+import { Request, Router } from 'express';
 import { handleRequest } from '@utils/handle-request';
 import { AuthMiddleware } from '@middlewares/auth.middleware';
 import { validateRequest } from '@middlewares/validate.middleware';
 import CuratorService from '@/services/curator.service';
 import UserRepository from '@/repositories/user.repository';
-import { getCuratorsSchema } from '@/domain/schemas/curator.schema';
+import { getCuratorDetailsSchema, getCuratorsSchema } from '@/domain/schemas/curator.schema';
 
 class CuratorController implements Controller {
   public path = '/curator';
@@ -20,11 +20,22 @@ class CuratorController implements Controller {
   }
 
   getCurators = async (req: Request) => {
-    const data = await this.curatorService.getCurators(req.body);
+    const data = await this.curatorService.getCurators(req.query);
+    return { data };
+  };
+
+  getCuratorDetails = async (req: Request) => {
+    const data = await this.curatorService.getCuratorDetails(req.params.username, req.query);
     return { data };
   };
   private initializeRoutes() {
-    this.router.get(`${this.path}/`, [this.authMiddleware.validatePhp], validateRequest(getCuratorsSchema), handleRequest(this.getCurators));
+    this.router.get(`${this.path}/`, [this.authMiddleware.validateApiKey], validateRequest(getCuratorsSchema), handleRequest(this.getCurators));
+    this.router.get(
+      `${this.path}/:username`,
+      [this.authMiddleware.validateApiKey],
+      validateRequest(getCuratorDetailsSchema),
+      handleRequest(this.getCuratorDetails),
+    );
   }
 }
 
