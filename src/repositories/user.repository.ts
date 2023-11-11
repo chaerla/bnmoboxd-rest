5bd354109151292cb64df99d0094b028056a27f4
chaerla <chelgchen@gmail.com>
2023-11-11 19:07:37 +0700

    feat: add curator details method in repo

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,5 @@
 import prisma from '@/database/prisma';
+import { GetReviewsOptions } from '@/repositories/curator-review.repository';
 
 export interface IInsertUser {
   username: string;
@@ -12,6 +13,11 @@ interface GetUsersOptions {
   page?: number;
   take?: number;
 }
+
+export type GetCuratorDetailsOptions = GetReviewsOptions & {
+  reviews: 'true' | 'false';
+};
+
 class UserRepository {
   getUserById = async (id: number) => {
     return await prisma.user.findFirst({
@@ -59,9 +65,10 @@ class UserRepository {
     const where = {
       isAdmin: false,
     };
-    const [users, count] = await prisma.$transaction([
+    const [curators, count] = await prisma.$transaction([
       prisma.user.findMany({
         select: {
+          username: true,
           firstName: true,
           lastName: true,
           reviewCount: true,
@@ -74,8 +81,37 @@ class UserRepository {
         where,
       }),
     ]);
-    return { users, count };
+    return { curators, count };
   };
+
+  async getCuratorDetails(curatorUsername: string, options: GetCuratorDetailsOptions) {
+    const where = {
+      isAdmin: false,
+      username: curatorUsername,
+    };
+    console.log(where);
+    const take = options.take || 10;
+    const skip = options.page && options.page - 1 > 0 ? (options.page - 1) * take : 0;
+    const select = {
+      id: true,
+      username: true,
+      firstName: true,
+      lastName: true,
+      reviewCount: true,
+    };
+
+    if (options.reviews === 'true') {
+      select['Review'] = {
+        take,
+        skip,
+      };
+    }
+
+    return await prisma.user.findFirst({
+      select,
+      where,
+    });
+  }
 }
 
 export default UserRepository;
