39eebfb75a9a6d2f48673a1fdde5738e8828b16e
chaerla <chelgchen@gmail.com>
2023-11-11 19:05:29 +0700

    fix: removed is verified from reviews

---
+++ prisma/migrations/20231111083951_delete_is_verified_from_curator_reviews/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_verified` on the `curator_reviews` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "curator_reviews" DROP COLUMN "is_verified";
